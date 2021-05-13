228574875c8adb8e853b3ff436ecbd2db7c2c69f
emefde <mateuszfidos@gmail.com>
2021-05-13 13:12:24 +0200

    DI modules work

--- DI/DependencyInjection.playground/Contents.swift
+++ DI/DependencyInjection.playground/Contents.swift
@@ -5,31 +5,93 @@ import Foundation
 
 
 protocol Registry {
-    func register<T>(type: T.Type, service: Any)
+    func register<T>(type: T.Type, service: T)
 }
 
 protocol Resolver {
-    func resolve<T>(_ type: T.Type) -> T
+    func resolve<T>(type: T.Type) -> T?
+}
+
+protocol KeyIdentifiable {
+    func key<T>(_ type: T.Type) -> ObjectIdentifier
 }
 
 
-typealias Locator = Registry & Resolver
+typealias Container = Registry & Resolver & KeyIdentifiable
 
 protocol ModuleConfiguration {
 
     func register(using registry: Registry)
-    func start(using resolver: Resolver)
+    func start(with resolver: Resolver) throws
 
 }
 
-class ExampleModuleConfiguration: ModuleConfiguration {
+final class AppDependencyContainer: Container {
 
-    func register(using registry: Registry) {
-        <#code#>
+    private var dependencies: [ObjectIdentifier: Any] = [:]
+
+    func register<T>(type: T.Type, service: T) {
+        dependencies[key(type)] = service
     }
 
-    func start(using resolver: Resolver) {
-        <#code#>
+    func resolve<T>(type: T.Type) -> T? {
+        dependencies[key(type)] as? T
+    }
+
+    func key<T>(_ type: T.Type) -> ObjectIdentifier {
+        ObjectIdentifier(type)
+    }
+
+}
+
+protocol NetworkingService { }
+final class NetworkingAdapter: NetworkingService { }
+
+
+final class ProfilesModuleConfiguration: ModuleConfiguration {
+
+    enum ResolverError: Error {
+        case couldNotResolveDependency
+        var localizedDescription: String {
+            switch self {
+            case .couldNotResolveDependency:
+                return "Resolver could not resolve the dependency. All dependencies MUST be set up properly"
+            }
+        }
+    }
+
+    private(set) var networkingService: NetworkingService!
+
+    func register(using registry: Registry) {
+        registry.register(type: NetworkingService.self, service: NetworkingAdapter())
+    }
+
+    func start(with resolver: Resolver) throws {
+        guard let networkingService = resolver.resolve(type: NetworkingService.self)
+        else {
+            throw ResolverError.couldNotResolveDependency
+        }
+
+        self.networkingService = networkingService
+    }
+
+}
+
+import UIKit
+
+//main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    private let container = AppDependencyContainer()
+    private var modules: [ModuleConfiguration] = [ProfilesModuleConfiguration()]
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?,
+        window: UIWindow?
+    ) {
+        modules.forEach { $0.register(using: container)}
+        modules.forEach { try? $0.start(with: container) }
     }
 
 }
